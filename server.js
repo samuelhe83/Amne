c00106fe62e96516cf77e5b0f260161f42957dea
Samuel He <samuelhe83@gmail.com>
2017-08-25 16:52:05 -0700

    Add Procfile

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+  res.render(path.resolve(__dirname, 'index.html'));
 });
